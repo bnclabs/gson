bff0cf8008abab324851a4ba426f0841a6449914
prataprc <prataprc@gmail.com>
2015-07-11 00:55:22 +0530

    abs for integers.

--- util.go
+++ util.go
@@ -20,3 +20,23 @@ func str2bytes(str string) []byte {
 	sl := &reflect.SliceHeader{Data: st.Data, Len: st.Len, Cap: st.Len}
 	return *(*[]byte)(unsafe.Pointer(sl))
 }
+
+func absInt8(val int8) uint8 {
+	mask := val >> 7
+	return uint8((mask + val) ^ mask)
+}
+
+func absInt16(val int16) uint16 {
+	mask := val >> 15
+	return uint16((mask + val) ^ mask)
+}
+
+func absInt32(val int32) uint32 {
+	mask := val >> 31
+	return uint32((mask + val) ^ mask)
+}
+
+func absInt64(val int64) uint64 {
+	mask := val >> 63
+	return uint64((mask + val) ^ mask)
+}
