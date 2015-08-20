44a7631ba9d62b2c910e998f2dc0e502c9a2258f
prataprc <prataprc@gmail.com>
2015-08-20 09:03:43 +0530

    collate: initial version from github.com/prataprc/collatejson.
    for the archive of commits to collatejson please refer
    to the original source, which is now depricated and maintained
    only for archive purpose.

---
+++ collate/sort.go
@@ -0,0 +1,21 @@
+package collate
+
+import "bytes"
+
+// ByteSlices to implement Sort interface.
+type ByteSlices [][]byte
+
+func (b ByteSlices) Len() int {
+	return len(b)
+}
+
+func (b ByteSlices) Less(i, j int) bool {
+	if bytes.Compare(b[i], b[j]) > 0 {
+		return false
+	}
+	return true
+}
+
+func (b ByteSlices) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}
