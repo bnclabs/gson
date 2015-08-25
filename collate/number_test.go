b14ee92d06db8b7ad38c52bb1acf53b79065e5fe
prataprc <prataprc@gmail.com>
2015-08-25 15:03:29 +0530

    collate: refactor codec.go into number.go and string.go

--- collate/number_test.go
+++ collate/number_test.go
@@ -148,26 +148,6 @@ func TestLargeDecimal(t *testing.T) {
 	}
 }
 
-func TestSuffixCoding(t *testing.T) {
-	testcases := [][]byte{
-		[]byte("hello\x00wo\xffrld\x00"),
-		[]byte("hello\x00wo\xffrld\x00ln"),
-	}
-	for _, bs := range testcases {
-		code, text := make([]byte, 1024), make([]byte, 1024)
-		n := suffixEncodeString(bs, code)
-		code[n] = Terminator
-		n++
-		x, y := suffixDecodeString(code[:n], text)
-		if bytes.Compare(bs, text[:y]) != 0 {
-			t.Error("Suffix coding for strings failed")
-		}
-		if l := len(code[x:n]); l != 0 {
-			t.Errorf("Suffix coding for strings, residue found %v", l)
-		}
-	}
-}
-
 func BenchmarkEncodeInt0(b *testing.B) {
 	bEncodeInt(b, []byte("0"))
 }
@@ -435,26 +415,6 @@ func BenchmarkDecodeLD(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		decodeLD(samples[i%ln], text[:])
-	}
-}
-
-func BenchmarkSuffixEncode(b *testing.B) {
-	var code [1024]byte
-	bs := []byte("hello\x00wo\xffrld\x00")
-	for i := 0; i < b.N; i++ {
-		suffixEncodeString(bs, code[:])
-	}
-}
-
-func BenchmarkSuffixDecode(b *testing.B) {
-	var code, text [1024]byte
-	bs := []byte("hello\x00wo\xffrld\x00")
-	n := suffixEncodeString(bs, code[:])
-	code[n] = Terminator
-	n++
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		suffixDecodeString(code[:n], text[:])
 	}
 }
 
---
+++ collate/string.go
@@ -0,0 +1,54 @@
+package collate
+
+func suffixEncodeString(s []byte, code []byte) int {
+	n := 0
+	for _, x := range s {
+		code[n] = x
+		n++
+		if x == Terminator {
+			code[n] = 1
+			n++
+		}
+	}
+	code[n] = Terminator
+	return n + 1
+}
+
+func suffixDecodeString(code []byte, text []byte) (int, int) {
+	for i, j := 0, 0; i < len(code); i++ {
+		x := code[i]
+		if x == Terminator {
+			i++
+			switch x = code[i]; x {
+			case 1:
+				text[j] = 0
+				j++
+			case Terminator:
+				return i + 1, j
+			default:
+				panic(ErrorSuffixDecoding)
+			}
+			continue
+		}
+		text[j] = x
+		j++
+	}
+	panic(ErrorSuffixDecoding)
+}
+
+func encodedStringSize(code []byte) int {
+	for i := 0; i < len(code); {
+		if code[i] == Terminator {
+			switch code[i+1] {
+			case 1:
+				i++
+			case Terminator:
+				return i
+			default:
+				panic(ErrorSuffixDecoding)
+			}
+		}
+		i++
+	}
+	panic(ErrorSuffixDecoding)
+}
