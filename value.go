e7c61c61759e3d5905247ab2fbb4d28e02f5c8b8
prataprc <prataprc@gmail.com>
2016-10-25 13:39:44 +0530

    collation algorithm on golang values (JSON confirming).

--- value.go
+++ value.go
@@ -1,5 +1,9 @@
 package gson
 
+import "bytes"
+import "sort"
+import "fmt"
+
 // Value abstractions for golang-native value.
 type Value struct {
 	config *Config
@@ -78,4 +82,266 @@ func (val *Value) Append(jptr *Jsonpointer, item interface{}) interface{} {
 //      newval, _ = val.Append("", "welcome")
 func (val *Value) Prepend(jptr *Jsonpointer, item interface{}) interface{} {
 	return valPrepend(jptr.Segments(), val.data, item)
+}
+
+func (val *Value) Compare(other *Value) int {
+	return valuecompare(val.data, other.data)
+}
+
+func valuecompare(v1, v2 interface{}) int {
+	t1, t2 := valueType(v1), valueType(v2)
+	if t1 < t2 {
+		return -1
+
+	} else if t1 > t2 {
+		return 1
+
+	} else {
+		switch t1 {
+		case TypeNull:
+			return 0
+
+		case TypeFalse:
+			if t2 == TypeTrue {
+				return -1
+			}
+			return 0
+
+		case TypeTrue:
+			if t2 == TypeFalse {
+				return 1
+			}
+			return 0
+
+		case TypeNumber:
+			cmp := cmpnumber(v1, v2)
+			return cmp
+
+		case TypeString:
+			b1 := str2bytes(v1.(string))
+			b2 := str2bytes(v2.(string))
+			return bytes.Compare(b1, b2)
+
+		case TypeArray:
+			a1, a2 := v1.([]interface{}), v2.([]interface{})
+			for i, item := range a1 {
+				if i > len(a2)-1 {
+					break
+				} else if cmp := valuecompare(item, a2[i]); cmp != 0 {
+					return cmp
+				}
+			}
+			if len(a1) < len(a2) {
+				return -1
+			} else if len(a1) > len(a2) {
+				return 1
+			}
+			return 0
+
+		case TypeObj:
+			o1 := v1.(map[string]interface{})
+			o2 := v2.(map[string]interface{})
+			switch {
+			case len(o1) < len(o2):
+				return -1
+			case len(o1) > len(o2):
+				return 1
+			}
+
+			keys1 := make([]string, 0)
+			for key := range o1 {
+				keys1 = append(keys1, key)
+			}
+			sort.Strings(keys1)
+			keys2 := make([]string, 0)
+			for key := range o2 {
+				keys2 = append(keys2, key)
+			}
+			sort.Strings(keys2)
+
+			for i, key1 := range keys1 {
+				key2 := keys2[i]
+				val1, val2 := o1[key1], o2[key2]
+				switch {
+				case key1 < key2:
+					return -1
+				case key1 > key2:
+					return 1
+				case key1 == key2:
+					cmp := valuecompare(val1, val2)
+					if cmp != 0 {
+						return cmp
+					}
+				}
+			}
+			return 0
+		}
+	}
+	panic(fmt.Errorf("unknown value type: %T", v1))
+}
+
+func valueType(data interface{}) byte {
+	if data == nil {
+		return TypeNull
+	}
+	switch v := data.(type) {
+	case bool:
+		if v == false {
+			return TypeFalse
+		}
+		return TypeTrue
+	case float64:
+		return TypeNumber
+	case int64, uint64, int, uint:
+		return TypeNumber
+	case string:
+		return TypeString
+	case []interface{}:
+		return TypeArray
+	case map[string]interface{}:
+		return TypeObj
+	}
+	panic(fmt.Errorf("unknown value type: %T", data))
+}
+
+func cmpnumber(n1, n2 interface{}) int {
+	switch v1 := n1.(type) {
+	case float64:
+		return float64t(v1).cmp(n2)
+	case int:
+		return int64t(v1).cmp(n2)
+	case int64:
+		return int64t(v1).cmp(n2)
+	case uint:
+		return uint64t(v1).cmp(n2)
+	case uint64:
+		return uint64t(v1).cmp(n2)
+	}
+	panic(fmt.Errorf("unknown value type: %T", n1))
+}
+
+type float64t float64
+
+func (this float64t) cmp(other interface{}) int {
+	var rc int
+	switch {
+	case float64(this) < float64(int64(this)):
+		rc = -1
+	case float64(this) > float64(int64(this)):
+		rc = 1
+	}
+
+	switch val := other.(type) {
+	case float64:
+		if float64(this) < val {
+			return -1
+		} else if float64(this) == val {
+			return 0
+		}
+		return 1
+
+	case int:
+		if v := int64(this); v < int64(val) {
+			return -1
+		} else if v == int64(val) {
+			return rc
+		}
+		return 1
+
+	case int64:
+		if v := int64(this); v < val {
+			return -1
+		} else if v == val {
+			return rc
+		}
+		return 1
+
+	case uint:
+		if v := uint64(this); v < uint64(val) {
+			return -1
+		} else if v == uint64(val) {
+			return rc
+		}
+		return 1
+
+	case uint64:
+		if v := uint64(this); v < val {
+			return -1
+		} else if v == val {
+			return rc
+		}
+		return 1
+	}
+	panic(fmt.Errorf("cannot handle type %T", other))
+}
+
+type int64t int64
+
+func (this int64t) cmp(other interface{}) int {
+	switch val := other.(type) {
+	case float64:
+		return -float64t(val).cmp(int64(this))
+	case int:
+		if int64(this) < int64(val) {
+			return -1
+		} else if int64(this) == int64(val) {
+			return 0
+		}
+		return 1
+
+	case int64:
+		if int64(this) < val {
+			return -1
+		} else if int64(this) == val {
+			return 0
+		}
+		return 1
+
+	case uint:
+		if uint64(val) >= 9223372036854775808 {
+			return -1
+		} else if v := int64(val); this < int64t(v) {
+			return -1
+		} else if this == int64t(v) {
+			return 0
+		}
+		return 1
+
+	case uint64:
+		if val >= 9223372036854775808 {
+			return -1
+		} else if v := int64(val); this < int64t(v) {
+			return -1
+		} else if this == int64t(v) {
+			return 0
+		}
+		return 1
+	}
+	panic(fmt.Errorf("cannot handle type %T", other))
+}
+
+type uint64t int64
+
+func (this uint64t) cmp(other interface{}) int {
+	switch val := other.(type) {
+	case float64:
+		return -float64t(val).cmp(uint64(this))
+	case int64:
+		if val < 0 {
+			return -1
+		} else if v := int64(val); int64(this) < v {
+			return -1
+		} else if int64(this) == v {
+			return 0
+		}
+		return 1
+	case uint64:
+		if uint64(this) < val {
+			return -1
+		} else if uint64(this) == val {
+			return 0
+		}
+		return 1
+	}
+	panic(fmt.Errorf("cannot handle type %T", other))
 }
---
+++ value_test.go
@@ -0,0 +1,147 @@
+package gson
+
+import "testing"
+import "bytes"
+import "sort"
+import "io/ioutil"
+import "strings"
+import "path"
+import "fmt"
+
+var _ = fmt.Sprintf("dummy")
+
+func TestValueCompare(t *testing.T) {
+	config := NewDefaultConfig()
+	testcases := [][3]interface{}{
+		[3]interface{}{nil, nil, 0},
+		[3]interface{}{nil, false, -1},
+		[3]interface{}{nil, true, -1},
+		[3]interface{}{nil, 10, -1},
+		[3]interface{}{nil, "hello", -1},
+		[3]interface{}{nil, []interface{}{10, 20}, -1},
+		[3]interface{}{nil, map[string]interface{}{"key1": 10}, -1},
+		[3]interface{}{false, nil, 1},
+		[3]interface{}{false, false, 0},
+		[3]interface{}{false, true, -1},
+		[3]interface{}{false, 10, -1},
+		[3]interface{}{false, "hello", -1},
+		[3]interface{}{false, []interface{}{10, 20}, -1},
+		[3]interface{}{false, map[string]interface{}{"key1": 10}, -1},
+		[3]interface{}{true, nil, 1},
+		[3]interface{}{true, false, 1},
+		[3]interface{}{true, true, 0},
+		[3]interface{}{true, 10, -1},
+		[3]interface{}{true, "hello", -1},
+		[3]interface{}{true, []interface{}{10, 20}, -1},
+		[3]interface{}{true, map[string]interface{}{"key1": 10}, -1},
+		[3]interface{}{10, nil, 1},
+		[3]interface{}{10, false, 1},
+		[3]interface{}{10, true, 1},
+		[3]interface{}{10, 10, 0},
+		[3]interface{}{10, "hello", -1},
+		[3]interface{}{10, []interface{}{10, 20}, -1},
+		[3]interface{}{10, map[string]interface{}{"key1": 10}, -1},
+		[3]interface{}{[]interface{}{10}, nil, 1},
+		[3]interface{}{[]interface{}{10}, false, 1},
+		[3]interface{}{[]interface{}{10}, true, 1},
+		[3]interface{}{[]interface{}{10}, 10, 1},
+		[3]interface{}{[]interface{}{10}, "hello", 1},
+		[3]interface{}{[]interface{}{10}, []interface{}{10}, 0},
+		[3]interface{}{[]interface{}{10}, map[string]interface{}{"key1": 10}, -1},
+		[3]interface{}{map[string]interface{}{"key1": 10}, nil, 1},
+		[3]interface{}{map[string]interface{}{"key1": 10}, false, 1},
+		[3]interface{}{map[string]interface{}{"key1": 10}, true, 1},
+		[3]interface{}{map[string]interface{}{"key1": 10}, 10, 1},
+		[3]interface{}{map[string]interface{}{"key1": 10}, "hello", 1},
+		[3]interface{}{map[string]interface{}{"key1": 10}, []interface{}{10}, 1},
+		[3]interface{}{map[string]interface{}{"key1": 10},
+			map[string]interface{}{"key1": 10}, 0},
+	}
+	for _, tcase := range testcases {
+		val1 := config.NewValue(tcase[0])
+		val2 := config.NewValue(tcase[1])
+		ref, cmp := tcase[2].(int), val1.Compare(val2)
+		if cmp != ref {
+			t.Errorf("for nil expected %v, got %v", ref, cmp)
+		}
+	}
+}
+
+func TestValueCollate(t *testing.T) {
+	dirname := "testdata/collate"
+	entries, err := ioutil.ReadDir(dirname)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, entry := range entries {
+		file := path.Join(dirname, entry.Name())
+		if !strings.HasSuffix(file, ".ref") {
+			out := strings.Join(collatefile(file), "\n")
+			ref, err := ioutil.ReadFile(file + ".ref")
+			if err != nil {
+				t.Fatal(err)
+			}
+			if strings.Trim(string(ref), "\n") != out {
+				//fmt.Println(string(ref))
+				//fmt.Println(string(out))
+				t.Fatalf("sort mismatch in %v", file)
+			}
+		}
+	}
+}
+
+func collatefile(filename string) (outs []string) {
+	s, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err.Error())
+	}
+	config := NewDefaultConfig()
+	if strings.Contains(filename, "numbers") {
+		config = config.SetNumberKind(SmartNumber)
+	}
+	return collateLines(config, s)
+}
+
+func collateLines(config *Config, s []byte) []string {
+	texts, values := lines(s), make(valueList, 0)
+	for i, text := range texts {
+		jsn := config.NewJson(text, -1)
+		_, val := jsn.Tovalue()
+		values = append(values, valObj{i, config.NewValue(val)})
+	}
+	outs := doSort(texts, values)
+	return outs
+}
+
+func doSort(texts [][]byte, values valueList) (outs []string) {
+	sort.Sort(values)
+	outs = make([]string, 0)
+	for _, value := range values {
+		outs = append(outs, string(texts[value.off]))
+	}
+	return
+}
+
+func lines(content []byte) [][]byte {
+	content = bytes.Trim(content, "\r\n")
+	return bytes.Split(content, []byte("\n"))
+}
+
+type valObj struct {
+	off int
+	val *Value
+}
+
+type valueList []valObj
+
+func (values valueList) Len() int {
+	return len(values)
+}
+
+func (values valueList) Less(i, j int) bool {
+	return values[i].val.Compare(values[j].val) < 0
+}
+
+func (values valueList) Swap(i, j int) {
+	values[i], values[j] = values[j], values[i]
+}
