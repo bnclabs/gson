6de4259c8cd4d843ed5f6cf4538dfa62df8edd8f
prataprc <prataprc@gmail.com>
2017-06-22 20:36:24 +0530

    cmd: gofmt -s.

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -17,86 +17,86 @@ var updateref = false
 func TestCmdArgs(t *testing.T) {
 	testcases := [][]interface{}{
 		// json transformations
-		[]interface{}{
+		{
 			[]string{"-inpfile", "example.json", "-json2value"},
 			[]byte("Json: \"hello world\"\nValu: hello world\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inptxt", `"hello world"`, "-json2value"},
 			[]byte("Json: \"hello world\"\nValu: hello world\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inpfile", "example.json", "-json2cbor"},
 			[]byte("Json: \"hello world\"\n" +
 				"Cbor: [107 104 101 108 108 111 32 119 111 114 108 100]\n" +
 				"Cbor: \"khello world\"\n" +
 				"Json: \"hello world\"\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inptxt", `"hello world"`, "-json2cbor"},
 			[]byte("Json: \"hello world\"\n" +
 				"Cbor: [107 104 101 108 108 111 32 119 111 114 108 100]\n" +
 				"Cbor: \"khello world\"\n" +
 				"Json: \"hello world\"\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inpfile", "example.json", "-json2collate"},
 			[]byte("Json: \"hello world\"\n" +
 				"Coll: \"\\x06hello world\\x00\\x00\"\n" +
 				"Coll: [6 104 101 108 108 111 32 119 111 114 108 100 0 0]\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inptxt", `"hello world"`, "-json2collate"},
 			[]byte("Json: \"hello world\"\n" +
 				"Coll: \"\\x06hello world\\x00\\x00\"\n" +
 				"Coll: [6 104 101 108 108 111 32 119 111 114 108 100 0 0]\n"),
 		},
 		// json options
-		[]interface{}{
+		{
 			[]string{"-inpfile", "../testdata/typical.json", "-pointers"},
 			testdataFile("../testdata/typical_pointers"),
 		},
 		// cbor transformations
-		[]interface{}{
+		{
 			[]string{"-inpfile", "example.cbor", "-cbor2value"},
 			[]byte("Cbor: \"khello world\"\n" +
 				"Cbor: [107 104 101 108 108 111 32 119 111 114 108 100]\n" +
 				"Valu: hello world\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inptxt", "khello world", "-cbor2value"},
 			[]byte("Cbor: \"khello world\"\n" +
 				"Cbor: [107 104 101 108 108 111 32 119 111 114 108 100]\n" +
 				"Valu: hello world\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inpfile", "example.cbor", "-cbor2json"},
 			[]byte("Cbor: \"khello world\"\nJson: \"hello world\"\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inptxt", "khello world", "-cbor2json"},
 			[]byte("Cbor: \"khello world\"\nJson: \"hello world\"\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inpfile", "example.cbor", "-cbor2collate"},
 			[]byte("Cbor: \"khello world\"\n" +
 				"Coll: \"\\x06hello world\\x00\\x00\"\n" +
 				"Coll: [6 104 101 108 108 111 32 119 111 114 108 100 0 0]\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inptxt", "khello world", "-cbor2collate"},
 			[]byte("Cbor: \"khello world\"\n" +
 				"Coll: \"\\x06hello world\\x00\\x00\"\n" +
 				"Coll: [6 104 101 108 108 111 32 119 111 114 108 100 0 0]\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inptxt", "khello world", "-cbor2collate"},
 			[]byte("Cbor: \"khello world\"\n" +
 				"Coll: \"\\x06hello world\\x00\\x00\"\n" +
 				"Coll: [6 104 101 108 108 111 32 119 111 114 108 100 0 0]\n"),
 		},
 		// cbor options
-		[]interface{}{
+		{
 			[]string{"-inptxt", `[10,20]`, "-ct", "stream", "-json2cbor"},
 			[]byte(
 				"Json: [10,20]\n" +
@@ -104,7 +104,7 @@ func TestCmdArgs(t *testing.T) {
 					"Cbor: \"\\x9f\\xfb@$\\x00\\x00\\x00\\x00\\x00\\x00\\xfb@4\\x00\\x00\\x00\\x00\\x00\\x00\\xff\"\n" +
 					"Json: [10,20]\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inptxt", `[10,20]`, "-ct", "lenprefix", "-json2cbor"},
 			[]byte(
 				"Json: [10,20]\n" +
@@ -113,31 +113,31 @@ func TestCmdArgs(t *testing.T) {
 					"Json: [10,20]\n"),
 		},
 		// collate transformations
-		[]interface{}{
+		{
 			[]string{"-inpfile", "example.coll", "-collate2value"},
 			[]byte("Coll: \"\\x06hello world\\x00\\x00\"\nValu: hello world\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inpfile", "example.coll", "-collate2json"},
 			[]byte("Coll: \"\\x06hello world\\x00\\x00\"\nJson: \"hello world\"\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inpfile", "example.coll", "-collate2cbor"},
 			[]byte("Coll: \"\\x06hello world\\x00\\x00\"\n" +
 				"Cbor: \"khello world\"\n" +
 				"Cbor: [107 104 101 108 108 111 32 119 111 114 108 100]\n"),
 		},
 		// value transformations
-		[]interface{}{
+		{
 			[]string{"-inptxt", `"hello world"`, "-value2json"},
 			[]byte("Valu: hello world\nJson: \"hello world\"\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inptxt", `"hello world"`, "-value2cbor"},
 			[]byte("Valu: hello world\nCbor: \"khello world\"\n" +
 				"Cbor: [107 104 101 108 108 111 32 119 111 114 108 100]\n"),
 		},
-		[]interface{}{
+		{
 			[]string{"-inptxt", `"hello world"`, "-value2collate"},
 			[]byte("Valu: hello world\n" +
 				"Coll: \"\\x06hello world\\x00\\x00\"\n" +
